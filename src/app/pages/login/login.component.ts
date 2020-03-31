308728178ce3dda939036291aa63b012096f40ce
Hazy Fish <hazyfish@outlook.com>
2020-03-30 22:48:50 -0700

    change snack bar duration

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -13,7 +13,6 @@ export class LoginComponent implements OnInit {
 
   email: string;
   password: string;
-  auth = auth;
 
   constructor(
     private fireAuth: AngularFireAuth,
@@ -55,6 +54,6 @@ export class LoginComponent implements OnInit {
   }
 
   showMessage(message: string): void {
-    this.snackBar.open(message, null, { duration: 2000 });
+    this.snackBar.open(message, null, { duration: 3000 });
   }
 }
