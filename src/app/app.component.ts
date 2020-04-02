9384efe6f46d0ee0e60459266d8c6df0e1fd4d98
Hazy Fish <hazyfish@outlook.com>
2020-04-01 20:22:29 -0700

    rename project and change firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng9-firebase-test';
+  title = 'activitree-edu-platform';
 
   constructor() {}
 }
