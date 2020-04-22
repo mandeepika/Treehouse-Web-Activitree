2ff944d4c7c327a64542618496c6d665582ab6b6
Hazy Fish <hazyfish@outlook.com>
2020-04-21 21:42:27 -0700

    add dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { ProfileComponent } from './pages/profile/profile.component';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    ProfileComponent
+    ProfileComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
