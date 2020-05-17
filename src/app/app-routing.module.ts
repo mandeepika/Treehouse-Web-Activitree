8976b83a13b4461887fe50ff600f799069bd747c
Henry Yu <hazyfish@outlook.com>
2020-05-17 04:07:56 +0000

    Revert "Merge branch 'firestore' into 'master'"
    This reverts merge request !2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,13 @@ import { HomeComponent } from "./pages/home/home.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
-import { GamesComponent } from './pages/games/games.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'register/continue', redirectTo: 'profile' },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'games', component: GamesComponent }
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
--- src/app/models/game.ts
+++
@@ -1,5 +0,0 @@
-export interface Game {
-    id: string;
-    title: string;
-    description: string;
-}
