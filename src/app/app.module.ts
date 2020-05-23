69eba7f6139e8e5a06ceca7bd23d6f7e0e33f71d
Hazy Fish <hazyfish@outlook.com>
2020-05-23 16:51:42 -0700

    Remove duplicate import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,6 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { GamesComponent } from './pages/games/games.component';
 import { CreateProfileComponent } from './pages/create-profile/create-profile.component';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { QuizCalculusComponent } from './pages/quiz-calculus/quiz-calculus.component';
 
 @NgModule({
