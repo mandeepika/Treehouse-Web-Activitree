2a2d80c330597d1753dc6bde5903cdc3061feda2
kgovind12 <kgovind@ucdavis.edu>
2020-09-29 23:24:28 -0700

    snapshot added

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Item } from '../models/item';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,14 @@ export class ItemService {
   items: Observable<Item[]>
 
   constructor(public afs: AngularFirestore) {
-    this.items = this.afs.collection('todo-items').valueChanges();
+    //this.items = this.afs.collection('todo-items').valueChanges();
+    this.items = this.afs.collection('todo-items').snapshotChanges().map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      })
+    });
    }
 
    getItems(){
