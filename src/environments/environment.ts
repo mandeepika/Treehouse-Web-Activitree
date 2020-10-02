94af107e1690089be0611bda0dac4d3017432097
kgovind12 <kgovind@ucdavis.edu>
2020-10-02 16:48:50 -0700

    changed gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyARCRNQS6sUtR7b4NdVW9UnMl1knbOcT1s',
+    authDomain: 'activitree-edu.firebaseapp.com',
+    databaseURL: 'https://activitree-edu.firebaseio.com',
+    projectId: 'activitree-edu',
+    storageBucket: 'activitree-edu.appspot.com',
+    messagingSenderId: '895797489069',
+    appId: '1:895797489069:web:f3a16163dd43d3c1044524',
+    measurementId: 'G-R3GW98VFDY'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
