d4201cbcec3ee1df3b55ff32595d720da6813505
kgovind12 <kgovind@ucdavis.edu>
2020-10-02 16:47:16 -0700

    added env files

--- src/app/pages/connect/connect.component.ts
+++ src/app/pages/connect/connect.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ConnectService } from '../../services/connect.service';
-import { User } from '../../models/user';
-import { UserService } from '../../services/user.service';
-import { AngularFireAuth } from '@angular/fire/auth';
-
 
 @Component({
   selector: 'app-connect',
@@ -12,20 +7,9 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class ConnectComponent implements OnInit {
 
-  constructor(public auth: AngularFireAuth, private service: UserService, private connectservice: ConnectService) { }
-  users: User[];
-  user: User;
-  currentUser: firebase.User;
-  //highSchool: string;
+  constructor() { }
 
   ngOnInit(): void {
-    this.connectservice.getUsers().subscribe(users => {
-      this.auth.user.subscribe(user => {
-        this.users = users.filter(x => x.id !== user.uid);
-        console.log(this.users, user.uid);
-        
-      });
-    });
-    
   }
+
 }
