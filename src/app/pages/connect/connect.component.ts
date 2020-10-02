ee265906329f95b2e1ae625ade887c0e79dd98b1
kgovind12 <kgovind@ucdavis.edu>
2020-10-02 16:57:07 -0700

    fixed another blunder

--- src/app/pages/connect/connect.component.ts
+++ src/app/pages/connect/connect.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectService } from '../../services/connect.service';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-connect',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth, private service: UserService, private connectservice: ConnectService) { }
+  users: User[];
+  user: User;
+  currentUser: firebase.User;
+
 
   ngOnInit(): void {
+    this.connectservice.getUsers().subscribe(users => {
+      this.auth.user.subscribe(user => {
+        this.users = users.filter(x => x.id !== user.uid);
+        console.log(this.users, user.uid);
+      });
+    });
   }
 
 }
