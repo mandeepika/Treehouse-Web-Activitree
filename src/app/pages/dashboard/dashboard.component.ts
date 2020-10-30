fdbda47d3eda084ab60a3e61533f6dac482b3695
kgovind12 <kgovind@ucdavis.edu>
2020-10-29 22:05:05 -0700

    comments added for todo list

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { UserService } from 'src/app/services/user.service';
 import { User } from '../../models/user';
 import { ItemService } from '../../services/item.service';
 import { Item } from '../../models/item';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,21 +12,18 @@ import { Item } from '../../models/item';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
   currentUser: User;
   user: User;
   firebaseUser: firebase.User;
 
-  todos: Item[];
-  itemToEdit: Item;
-  priority: number;
+  todos: Item[];  // an array of todo items. This array contains Item objects.
 
-  newTask: Item = {
+  newItem: Item = { // this is an Item object already created. 
     id: '',
     title: '',
     done: false,
     editing: false,
-    listnum: 0
+    priority: 0
   }
 
 
@@ -40,6 +38,24 @@ export class DashboardComponent implements OnInit {
       this.service.get(user.uid).subscribe(user => this.user = user);
     });
     //this.getItems();
+  }
+
+  //function skeletons set up below. Edit will come later.
+
+  addItem(){
+    // set the properties for newItem here.
+    // title = the thing they entered in the input
+    // done = false
+    // editing = false
+    // priority = 1
+    // you can leave the id property blank.
+    // add this newItem to the todos[] array (figure out how to add an element to an array in typescript)
+    // call this.itemservice.addTask() to add the item to the firestore todo-items collection.
+  }
+
+  deleteItem(){
+    // remove the item from the array
+    // call this.itemservice.deleteTask() to remove the task from the firestore todo-items collection.
   }
 
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -3,7 +3,7 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Item } from '../models/item';
 import { Observable, of } from 'rxjs';
 import { switchMap, map, first, mergeMap } from "rxjs/operators";
-import { auth } from 'firebase/app';
+import { auth, functions } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
@@ -24,6 +24,21 @@ export class ItemService {
 
   constructor(public auth: AngularFireAuth, private service: UserService, public afs: AngularFirestore) {
 
+  // This is the file that interacts with firestore. Here, you can write add, delete, and update functions.
+  // I've set up skeletons for them below.
+
+  //addTask(){
+    //add an item to the todo-items collection here.
+  //}
+
+  //deleteTask(){
+    //delete an item from the todo-items collection here.
+  //}
+
+  //This is how you can access the todo-items collection for now:. (That long string is a temporary user id).
+  //Later, we'll have to change it to the current user's ID
+  //afs.collection('users').doc('0Q5j9TOYHhWmTjAFtNok1zMllJ82').collection('todo-items');
+  
 }
 
 }
